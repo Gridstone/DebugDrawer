95e30131da458576311101bf1838ac50cf668300
Christopher Horner <horner.cd@gmail.com>
2018-08-30 17:06:32 +1000

    Allow debug drawer to react to window insets.

--- debug-drawer/src/main/kotlin/au/com/gridstone/debugdrawer/DebugDrawer.kt
+++ debug-drawer/src/main/kotlin/au/com/gridstone/debugdrawer/DebugDrawer.kt
@@ -3,10 +3,16 @@ package au.com.gridstone.debugdrawer
 import android.app.Activity
 import android.graphics.Color
 import android.view.Gravity.END
+import android.view.View
+import android.view.View.OnApplyWindowInsetsListener
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.view.WindowInsets
 import android.widget.FrameLayout
-import android.widget.ScrollView
+import android.widget.LinearLayout
+import android.widget.LinearLayout.VERTICAL
+import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 
 object DebugDrawer {
@@ -25,18 +31,43 @@ object DebugDrawer {
     }
 
     fun finishAndGetMainContainer(): ViewGroup {
+      // Create DrawerLayout and add mainContainer as its first child.
       val drawerLayout = DrawerLayout(activity)
       drawerLayout.addView(mainContainer)
 
-      val drawerContentContainer = ScrollView(activity)
+      // Create the ScrollView that will house the debug drawer content and add it to DrawerLayout.
+      val drawerContentScrollView = DrawerScrollView(activity)
       val params = DrawerLayout.LayoutParams(drawerLayout.dpToPx(290), MATCH_PARENT, END)
-      drawerContentContainer.layoutParams = params
-      drawerContentContainer.setBackgroundColor(Color.rgb(66, 66, 66))
-      drawerLayout.addView(drawerContentContainer)
+      drawerContentScrollView.layoutParams = params
+      drawerContentScrollView.setBackgroundColor(Color.rgb(66, 66, 66))
+      drawerLayout.addView(drawerContentScrollView)
 
+      // Create and add the drawer content container to the scroll view.
+      val drawerContent = LinearLayout(activity)
+      drawerContent.layoutParams = LinearLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT)
+      drawerContent.orientation = VERTICAL
+      drawerContent.addView(TextView(activity).apply { setText("Hello") })
+      drawerContentScrollView.addView(drawerContent)
 
+      // If the main container is dealing with window insets then we want to know. It will
+      // potentially allow us to render the debug drawer behind the status bar.
+      val insetListener = InsetListener(drawerContentScrollView, drawerContent)
+      mainContainer.setOnApplyWindowInsetsListener(insetListener)
+
+      //
       activity.setContentView(drawerLayout)
       return mainContainer
+    }
+  }
+
+  private class InsetListener(
+      private val scrollView: DrawerScrollView,
+      private val drawerContentContainer: View) : OnApplyWindowInsetsListener {
+
+    override fun onApplyWindowInsets(v: View, insets: WindowInsets): WindowInsets {
+      scrollView.setStatusBarHeight(insets.systemWindowInsetTop)
+      drawerContentContainer.setPadding(0, insets.systemWindowInsetTop, 0, 0)
+      return insets
     }
   }
 }

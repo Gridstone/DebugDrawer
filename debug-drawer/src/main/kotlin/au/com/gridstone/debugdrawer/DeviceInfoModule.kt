b5cab2ed3dcff680af82287294a5f1f9abce085a
Christopher Horner <horner.cd@gmail.com>
2018-08-31 10:00:19 +1000

    Begin calculation of screen density.

--- debug-drawer/src/main/kotlin/au/com/gridstone/debugdrawer/DeviceInfoModule.kt
+++ debug-drawer/src/main/kotlin/au/com/gridstone/debugdrawer/DeviceInfoModule.kt
@@ -1,11 +1,39 @@
 package au.com.gridstone.debugdrawer
 
 import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import android.util.DisplayMetrics.DENSITY_HIGH
+import android.util.DisplayMetrics.DENSITY_LOW
+import android.util.DisplayMetrics.DENSITY_MEDIUM
+import android.util.DisplayMetrics.DENSITY_TV
+import android.util.DisplayMetrics.DENSITY_XHIGH
+import android.util.DisplayMetrics.DENSITY_XXHIGH
+import android.util.DisplayMetrics.DENSITY_XXXHIGH
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 
 class DeviceInfoModule(context: Context) : DebugDrawerModule {
   override val title: String = context.getString(R.string.drawer_deviceInfo_title)
 
-  override fun onCreateView(parent: ViewGroup): View = parent.inflate(R.layout.device_info_module)
+  override fun onCreateView(parent: ViewGroup): View {
+
+    val view: View = parent.inflate(R.layout.device_info_module)
+    val resources: Resources = parent.context.resources
+    val displayMetrics: DisplayMetrics = resources.displayMetrics
+
+    val density: String = when (displayMetrics.densityDpi) {
+      DENSITY_LOW -> "ldpi"
+      DENSITY_MEDIUM -> "mdpi"
+      DENSITY_HIGH -> "hdpi"
+      DENSITY_TV -> "tvdpi"
+      DENSITY_XHIGH -> "xhdpi"
+      DENSITY_XXHIGH -> "xxhdpi"
+      DENSITY_XXXHIGH -> "xxxhdpi"
+      else -> "unknown"
+    }
+
+    return view
+  }
 }

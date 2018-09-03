909a9fdcb18fe6c615d1e21cb339a3e033c994c8
Christopher Horner <horner.cd@gmail.com>
2018-09-03 16:02:34 +1000

    Add GiantBomb API to sample app.

---
+++ sample-app/src/main/kotlin/au/com/gridstone/debugdrawer/sampleapp/GamesApi.kt
@@ -0,0 +1,61 @@
+package au.com.gridstone.debugdrawer.sampleapp
+
+import kotlinx.coroutines.experimental.Deferred
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+object GamesApi {
+
+  private const val API_URL = "https://www.giantbomb.com/api/"
+  private const val API_KEY = "8ca8e06cb445b8c055442108161848001a1c66fc"
+
+  private val client = OkHttpClient.Builder()
+      .addInterceptor(AuthInterceptor)
+      .build()
+
+  private val api: GiantBombApi = Retrofit.Builder()
+      .baseUrl(API_URL)
+      .client(client)
+      .addConverterFactory(MoshiConverterFactory.create())
+      .build()
+      .create(GiantBombApi::class.java)
+
+  suspend fun getGames(): GamesResult {
+
+    @Suppress("LiftReturnOrAssignment") // Keep things readable.
+    try {
+      val response: GamesResponse = api.getGames().await()
+      return GamesResult(true, response.results)
+    } catch (exception: Exception) {
+      return GamesResult(false, emptyList())
+    }
+  }
+
+  private data class GamesResponse(val results: List<Game>)
+
+  private interface GiantBombApi {
+    @GET("games?format=json&filter=name:zelda&field_list=id,name,image,aliases,deck,original_release_date,platforms")
+    fun getGames(): Deferred<GamesResponse>
+  }
+
+  private object AuthInterceptor : Interceptor {
+
+    override fun intercept(chain: Chain): okhttp3.Response {
+      val url: HttpUrl = chain.request().url().newBuilder()
+          .addQueryParameter("api_key", API_KEY)
+          .build()
+
+      val request: Request = chain.request().newBuilder()
+          .url(url)
+          .build()
+
+      return chain.proceed(request)
+    }
+  }
+}
---
+++ sample-app/src/main/kotlin/au/com/gridstone/debugdrawer/sampleapp/Models.kt
@@ -0,0 +1,15 @@
+package au.com.gridstone.debugdrawer.sampleapp
+
+data class GamesResult(val success: Boolean, val games: List<Game>)
+
+data class Game(val id: Long,
+                val name: String,
+                val aliases: String?,
+                val deck: String,
+                val image: Image,
+                val original_release_date: String,
+                val platforms: List<Platform>)
+
+data class Platform(val name: String)
+
+data class Image(val small_url: String, val super_url: String)

2df1b1e9bcbf4ac8c45eca5869aa4c917e2d083b
xxfast <isuru@xfastgames.com>
2018-10-29 11:52:17 +1100

    Add pretty print for the http logger in the sample app

--- debugdrawer-okhttp-logger/src/main/kotlin/au/com/gridstone/debugdrawer/HttpLogger.kt
+++ debugdrawer-okhttp-logger/src/main/kotlin/au/com/gridstone/debugdrawer/HttpLogger.kt
@@ -7,6 +7,9 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import timber.log.Timber
 
 /**
@@ -16,9 +19,23 @@ import timber.log.Timber
  * You must add [interceptor] to your client via [OkHttpClient.Builder.addInterceptor] in order for
  * logs to be intercepted and delivered to Timber.
  */
-class HttpLogger(context: Context) {
+class HttpLogger(context: Context, prettyPrintJson: Boolean = true) {
 
-  private val logInterceptor = HttpLoggingInterceptor { message -> Timber.tag("HTTP").v(message) }
+  private val logInterceptor = HttpLoggingInterceptor { message ->
+    val formattedMessage: String = try {
+      when {
+        !prettyPrintJson -> message
+        message.startsWith('{') -> JSONObject(message).toString(2)
+        message.startsWith('[') -> JSONArray(message).toString(2)
+        else -> message
+      }
+    } catch (e: JSONException) {
+      message
+    }
+    
+    Timber.tag("HTTP").v(formattedMessage)
+  }
+
   private val sharedPrefs: SharedPreferences = context
       .getSharedPreferences("DebugDrawer_HttpLogger", MODE_PRIVATE)
 
